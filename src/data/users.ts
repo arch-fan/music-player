52abea2102161d3c0318fa871291598ef8d0269e
arch-fan <juanserracines@gmail.com>
2024-05-10 15:50:34 +0200

    Change icon

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,4 +1,4 @@
 export const users: Record<string, string> = {
-	Juan: "https://pbs.twimg.com/profile_images/1769835941856702464/AgNNCmyy_400x400.jpg",
+	Juan: "https://p16-capcut-sign-va.ibyteimg.com/tos-maliva-v-be9c48-us/1e5fb246a1654c1b89137e173ccfc20b~tplv-nhvfeczskr-1:250:0.webp?lk3s=44acef4b&x-expires=1743536606&x-signature=Nr44ZLL0UG6V%2FCgCxULW4qpX3TY%3D",
 	Vicente: "/music-player/vicente.gif",
 };
